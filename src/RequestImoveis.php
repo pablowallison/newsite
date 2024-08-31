f0cd4d0afde63dc6776f91fd462f867000404d8c
Pablo Wallison <contato@pablowallison.com>
2024-08-30 23:19:37 -0400

    4

--- src/RequestImoveis.php
+++ src/RequestImoveis.php
@@ -15,50 +15,44 @@ class RequestImoveis {
         } elseif ($method === 'GET' && !empty($data)) {
             $url .= '?' . http_build_query($data);
         }
-
-        // Setando a URL e outras opções do cURL
+    
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  // Retorna a resposta como uma string
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            "Content-type: application/x-www-form-urlencoded",
-            $authorization ? "Authorization: $authorization" : ""
-        ]);
-        
-        // Desabilitar verificação SSL (não recomendado em produção)
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 
-
-        // Executando a requisição e pegando a resposta
+        $headers = ["Content-type: application/x-www-form-urlencoded"];
+        if ($authorization) {
+            $headers[] = "Authorization: $authorization";
+        }
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    
         $result = curl_exec($ch);
-
-        // Verificando por erros no cURL
+    
         if (curl_errno($ch)) {
             $error = curl_error($ch);
             curl_close($ch);
             throw new Exception("cURL error: $error");
         }
-
-        // Fechando o cURL
+    
         curl_close($ch);
-
-        // Decodificando a resposta JSON
+    
         $decodedResult = json_decode($result, true);
         if ($decodedResult === null) {
             throw new Exception("Error decoding JSON response: $result");
         }
-
+    
         return $decodedResult;
     }
+    
 
     public function loadAll() {
         try {
             // Parâmetros de filtro
-            $body = [
-                'limit'     => '3',
-                'order'     => 'id',
+            /*$body = [
+                'limit' => '3',
+                'order' => 'nome',
                 'direction' => 'desc'
-            ];
+            ];*/
 
             // URL da API que você quer acessar
             $location = 'https://painel.concretizaconstrucoes.com/rest.php?class=ImoveisRestService&method=LoadAll';
@@ -67,9 +61,7 @@ class RequestImoveis {
             $authorization = 'Basic 9fbbb2c765d1d5d12c1e3582a9329108c4ed9a96b199ffab6700a413869c';
 
             // Fazendo a requisição à API
-            $retorno = $this->request($location, 'GET', $body, $authorization);
-            
-            return $retorno;
+            return $this->request($location, 'GET', null, $authorization);
         } catch (Exception $e) {
             echo 'Error: ' . $e->getMessage();
             return null;
