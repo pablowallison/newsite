7462be073833a22f0fa525018d1be11acfe33b44
Pablo Wallison <contato@pablowallison.com>
2024-09-07 09:26:22 -0400

    ok

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ $action = isset($_GET['action']) ? htmlspecialchars($_GET['action']) : '';
 $param = array_merge($_GET, $_POST);
 
 $route->add('', function($args) use ($twig) {
-    header('Location: /');
+    header("Location: /home" );
     exit();
 });
 
